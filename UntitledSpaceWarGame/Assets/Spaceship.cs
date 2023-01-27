234b5d2be560179aefa2a98e51672eaf6f7b0f3b
Anas Peerzada <apeerzada987@gmail.com>
2023-01-26 22:56:03 -0500

    Added super basic spaceship sim

---
+++ UntitledSpaceWarGame/Assets/Spaceship.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Spaceship : MonoBehaviour
+{
+    [SerializeField] private GameObject cameraPoint;
+    [SerializeField] private Camera cameraThirdPerson;
+
+    [SerializeField] private GameObject shipModel;
+
+
+    private float shipSpeed = 10f;
+    private float shipTurnSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraFollow();
+        var forwardInput = -Input.GetAxis("Vertical");
+        var horizontalInput = Mathf.Clamp(Input.mousePosition.x/Screen.width, -1, 1);
+        var verticalInput = Mathf.Clamp(-Input.mousePosition.y/Screen.height, -1, 1);
+
+        transform.position += forwardInput * shipModel.transform.right * shipSpeed * Time.deltaTime;
+        transform.RotateAround(shipModel.transform.position, Vector3.up, horizontalInput*shipTurnSpeed*Time.deltaTime);
+        transform.RotateAround(shipModel.transform.position, Vector3.right, verticalInput*shipTurnSpeed*Time.deltaTime);
+
+    }
+
+    void CameraFollow()
+    {
+
+        cameraThirdPerson.transform.position = cameraPoint.transform.position;
+
+        cameraThirdPerson.transform.rotation = shipModel.transform.rotation * Quaternion.Euler(0f, -90, 0f);
+    }
+}
